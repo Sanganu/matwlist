8e08e499060f90b13f4b03d198ebb645f0fee706
scodingjs <anukppkms@gmail.com>
2017-11-10 22:30:35 -0700

    Configuration setup for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,26 @@
 var mysql = require("mysql");
+var connection;
 
+/*
 var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user:'root',
-  password:'Mykutties2',
+  password:'',
   database:'wishlist'
 });
+*/
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoadain_db'
+  });
+};
 
 connection.connect(function(err){
    if (err) throw err;
